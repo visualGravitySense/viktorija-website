b27b33df1ffba95e98e06673b681281ab38e33bb
visualGravitySense <helikeel@gmail.com>
2025-09-19 13:13:47 +0300

    Update Hero component background styles and localize student metrics: Change background gradients in the Hero component for improved aesthetics. Update student-related metrics in English, Estonian, and Russian localization files to reflect a new count of 1000+ students, enhancing social proof across the site.

--- src/components/marketing-page/components/Hero.tsx
+++ src/components/marketing-page/components/Hero.tsx
@@ -90,8 +90,8 @@ export default function Hero({
         width: '100%',
         backgroundImage:
           theme.palette.mode === 'light'
-            ? 'linear-gradient(180deg, #CEE5FD, #FFF)'
-            : `linear-gradient(#02294F, ${alpha('#090E10', 0.95)})`,
+            ? 'linear-gradient(180deg,rgba(49, 130, 206, 0.20), #FCFCFC)'
+            : `linear-gradient(#1F3B60, ${alpha('#05070A', 0.95)})`,
         backgroundSize: '100% 20%',
         backgroundRepeat: 'no-repeat',
       })}
